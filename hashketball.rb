9e454b05ab1eeb3e85905a578f6d32cc5e60c8bf
Candice Parker <lexus2424@gmail.com>
2019-05-08 22:50:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -188,6 +188,7 @@ end
         largest_shoe = 0
         game_hash.each do |location, data|
           data[:players].each do |player, player_hash|
+            binding.pry
             if data[:shoe] > largest_shoe
               return data[:rebounds]
             end
